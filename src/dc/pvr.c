d9f9f143ddc638fc6672cd4a0d3fd286bf4cb0dd
darc <darcagn@protonmail.com>
2025-02-02 21:33:57 -0600

    Eliminate warnings about discarding const qualifiers in C wrappers

--- src/dc/pvr.c
+++ src/dc/pvr.c
@@ -1,5 +1,5 @@
 // Rust for KallistiOS/Dreamcast
-// Copyright (C) 2024 Eric Fradella
+// Copyright (C) 2024, 2025 Eric Fradella
 // https://dreamcast.rs/
 
 #include <dc/pvr.h>
@@ -12,6 +12,6 @@ void pvr_set_pal_entry_wrapper(uint32_t idx, uint32_t value) {
     return pvr_set_pal_entry(idx, value);
 }
 
-void pvr_dr_commit_wrapper(const void *addr) {
+void pvr_dr_commit_wrapper(void *addr) {
     return pvr_dr_commit(addr);
 }
--- src/dc/sq.c
+++ src/dc/sq.c
@@ -1,9 +1,9 @@
 // Rust for KallistiOS/Dreamcast
-// Copyright (C) 2024 Eric Fradella
+// Copyright (C) 2024, 2025 Eric Fradella
 // https://dreamcast.rs/
 
 #include <dc/sq.h>
 
-void sq_flush_wrapper(const void *dest) {
+void sq_flush_wrapper(void *dest) {
     sq_flush(dest);
 }
