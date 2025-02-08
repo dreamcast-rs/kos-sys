03d68f059f71038becfaf10b5c8a5c38fecfb6eb
darc <darcagn@protonmail.com>
2025-02-08 14:01:21 -0600

    Change macro to const in aica_comm

--- src/dc/sound/aica_comm.rs
+++ src/dc/sound/aica_comm.rs
@@ -24,7 +24,7 @@ pub struct aica_cmd_t {
     cmd_data:       FAM<u8>,
 }
 
-pub const AICA_CMD_MAX_SIZE: c_size_t   = 256;
+pub const AICA_CMD_MAX_SIZE: c_size_t       = 256;
 
 #[repr(C)]
 pub struct aica_channel_t {
@@ -51,42 +51,38 @@ macro_rules! AICA_CMDSTR_CHANNEL {
     };
 }
 
-#[macro_export]
-macro_rules! AICA_CMDSTR_CHANNEL_SIZE {
-    () => {
-        (size_of::<aica_cmd_t>() + size_of::<aica_channel_t>()) / 4
-    }
-}
+pub const AICA_CMDSTR_CHANNEL_SIZE: usize   = (size_of::<aica_cmd_t>() +
+                                               size_of::<aica_channel_t>()) / 4;
 
-pub const AICA_CMD_NONE: u32            = 0x00000000;
-pub const AICA_CMD_PING: u32            = 0x00000001;
-pub const AICA_CMD_CHAN: u32            = 0x00000002;
-pub const AICA_CMD_SYNC_CLOCK: u32      = 0x00000003;
+pub const AICA_CMD_NONE: u32                = 0x00000000;
+pub const AICA_CMD_PING: u32                = 0x00000001;
+pub const AICA_CMD_CHAN: u32                = 0x00000002;
+pub const AICA_CMD_SYNC_CLOCK: u32          = 0x00000003;
 
-pub const AICA_RESP_NONE: u32           = 0x00000000;
-pub const AICA_RESP_PONG: u32           = 0x00000001;
-pub const AICA_RESP_DBGPRINT: u32       = 0x00000002;
+pub const AICA_RESP_NONE: u32               = 0x00000000;
+pub const AICA_RESP_PONG: u32               = 0x00000001;
+pub const AICA_RESP_DBGPRINT: u32           = 0x00000002;
 
-pub const AICA_CH_CMD_MASK: u32         = 0x0000000F;
+pub const AICA_CH_CMD_MASK: u32             = 0x0000000F;
 
-pub const AICA_CH_CMD_NONE: u32         = 0x00000000;
-pub const AICA_CH_CMD_START: u32        = 0x00000001;
-pub const AICA_CH_CMD_STOP: u32         = 0x00000002;
-pub const AICA_CH_CMD_UPDATE: u32       = 0x00000003;
+pub const AICA_CH_CMD_NONE: u32             = 0x00000000;
+pub const AICA_CH_CMD_START: u32            = 0x00000001;
+pub const AICA_CH_CMD_STOP: u32             = 0x00000002;
+pub const AICA_CH_CMD_UPDATE: u32           = 0x00000003;
 
 
-pub const AICA_CH_START_MASK: u32       = 0x00300000;
+pub const AICA_CH_START_MASK: u32           = 0x00300000;
 
-pub const AICA_CH_START_DELAY: u32      = 0x00100000;
-pub const AICA_CH_START_SYNC: u32       = 0x00200000;
+pub const AICA_CH_START_DELAY: u32          = 0x00100000;
+pub const AICA_CH_START_SYNC: u32           = 0x00200000;
 
-pub const AICA_CH_UPDATE_MASK: u32      = 0x000FF000;
+pub const AICA_CH_UPDATE_MASK: u32          = 0x000FF000;
 
-pub const AICA_CH_UPDATE_SET_FREQ: u32  = 0x00001000;
-pub const AICA_CH_UPDATE_SET_VOL: u32   = 0x00002000;
-pub const AICA_CH_UPDATE_SET_PAN: u32   = 0x00004000;
+pub const AICA_CH_UPDATE_SET_FREQ: u32      = 0x00001000;
+pub const AICA_CH_UPDATE_SET_VOL: u32       = 0x00002000;
+pub const AICA_CH_UPDATE_SET_PAN: u32       = 0x00004000;
 
-pub const AICA_SM_16BIT: u32            = 0;
-pub const AICA_SM_8BIT: u32             = 1;
-pub const AICA_SM_ADPCM: u32            = 2;
-pub const AICA_SM_ADPCM_LS: u32         = 3;
+pub const AICA_SM_16BIT: u32                = 0;
+pub const AICA_SM_8BIT: u32                 = 1;
+pub const AICA_SM_ADPCM: u32                = 2;
+pub const AICA_SM_ADPCM_LS: u32             = 3;
